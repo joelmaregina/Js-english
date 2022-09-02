ceee8b441e0652cf1c043c70685c2c6704f32ee8
Joelma Regina <joelma_silvaa@hotmail.com>
2022-09-02 15:51:32 -0300

    JS patterns: Module | Singleton | Factory | Observer

---
+++ JS patterns/patterns.js
@@ -0,0 +1,145 @@
+// *********** MODULE PATTERN - Basic structure ************
+
+// (function() {
+//   // Declare private vars and functions
+
+//   return {
+//     // Declare public var and functions
+//   }
+// })();
+
+// STANDARD MODULE PATTERN
+// const UICtrl = (function() {
+//   let text = 'Hello World';
+
+//   const changeText = function() {
+//     const element = document.querySelector('h1');
+//     element.textContent = text;
+//   }
+
+//   return {
+//     callChangeText: function() {
+//       changeText();
+//       // console.log(text);
+//     }
+//   }
+// })();
+
+// UICtrl.callChangeText();
+// // UICtrl.changeText();
+
+// console.log(UICtrl.text);
+
+// *********** REVEALING MODULE PATTERN ************
+const ItemCtrl = (function() {
+    let data = [];
+  
+    function add(item) {
+      data.push(item);
+      console.log('Item Added....');
+    }
+  
+    function get(id) {
+      return data.find(item => {
+        return item.id === id;
+      });
+    }
+  
+    return {
+      add: add,
+      // get: get
+    }
+  })();
+  
+  ItemCtrl.add({id: 1, name: 'John'});
+  ItemCtrl.add({id: 2, name: 'Mark'});
+  console.log(ItemCtrl.get(2));
+
+
+// ***************** SINGLETON PATTERN *****************
+// A manifestation of the module pattern (an immediate anonymous function) but can return only one instance of an object
+// As the module pattern it maintains a private reference, which nothing from the ouside can access
+
+  const Singleton = (function() {
+    let instance;
+  
+    function createInstance() {
+      const object = new Object({name:'Brad'});
+      return object;
+    }
+  
+    return {
+      getInstance: function() {
+        if(!instance){
+          instance = createInstance();
+        }
+        return instance;
+      }
+    }
+  })();
+  
+  const instanceA = Singleton.getInstance();
+  const instanceB = Singleton.getInstance();
+  
+  console.log(instanceA === instanceB);
+  
+  // console.log(instanceA);
+
+// ***************** OBSERVER PATTERN *******************
+
+function EventObserver() {
+    this.observers = [];
+  }
+  
+  EventObserver.prototype = {
+    subscribe: function(fn) {
+      this.observers.push(fn);
+      console.log(`You are now subscribed to ${fn.name}`);
+    },
+    unsubscribe: function(fn) {
+      // Filter out from the list whatever matches the callback function. If there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of observers.
+      this.observers = this.observers.filter(function(item){
+        if(item !== fn) {
+          return item;
+        }
+      });
+      console.log(`You are now unsubscribed from ${fn.name}`);
+    },
+    fire: function() {
+      this.observers.forEach(function(item) {
+        item.call();
+      });
+    }
+  }
+  
+  const click = new EventObserver();
+  
+  // Event Listeners
+  document.querySelector('.sub-ms').addEventListener('click', function() {
+    click.subscribe(getCurMilliseconds);
+  });
+  
+  document.querySelector('.unsub-ms').addEventListener('click', function() {
+    click.unsubscribe(getCurMilliseconds);
+  });
+  
+  document.querySelector('.sub-s').addEventListener('click', function() {
+    click.subscribe(getCurSeconds);
+  });
+  
+  document.querySelector('.unsub-s').addEventListener('click', function() {
+    click.unsubscribe(getCurSeconds);
+  });
+  
+  document.querySelector('.fire').addEventListener('click', function() {
+    click.fire();
+  });
+  
+  // Click Handler
+  const getCurMilliseconds = function() {
+    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+  }
+  
+  const getCurSeconds = function() {
+    console.log(`Current Seconds: ${new Date().getSeconds()}`);
+  }
